da9d54ccde210ec1cb45a475a8edb703cd261a22
Masturffect <ianseal10@gmail.com>
2022-02-01 20:47:03 -0500

    updated to use ref in InputWithLabel

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -20,24 +20,23 @@ const stories = [
   }
 ];
 
-  /*
-  this useEffect is used to trigger the side-effect
-  each time the searchTerm changes
-
-  useEffect params:
-    - function that runs side-effect
-    - dependency array of variables
-
-  important notes: 
-    - leaving out dependencies would make function
-        run on every render of component
-    - [] the function for side-effect is only called once
-  */
 
 const useSemiPersistentState = (key, initialState) => {
-  const [value, setValue] = useState(
-    localStorage.getItem(key) || initialState
-  );
+  const [value, setValue] = useState(localStorage.getItem(key) || initialState);
+
+    /*
+    this useEffect is used to trigger the side-effect
+    each time the searchTerm changes
+    
+    useEffect params:
+      - function that runs side-effect
+      - dependency array of variables
+    
+    important notes: 
+      - leaving out dependencies would make function
+          run on every render of component
+      - [] the function for side-effect is only called once
+    */
 
   useEffect(() => {
     localStorage.setItem(key, value);
@@ -105,13 +104,20 @@ const Item = ({item}) => (
   </li>
 );
 
-const InputWithLabel = ({ id, label, value, isFocused, type = 'text', onInputChange, children }) => {
-  /*
-    
-  */
+const InputWithLabel = ({ id, value, isFocused, type = 'text', onInputChange, children }) => {
+
+  // 1.) create a ref -- a ref object is a persistent value which stays
+  // intact over the lifetime of a React component. It comes with a prop called current,
+  // which, unlike the ref object, can be changed.
   const inputRef = useRef();
+
+  // 3.) opt into React's lifecycle w/ React's useEffect hook, which focuses on
+  // the input field when the component renders (or its dependencies change).
   useEffect(() => {
     if(isFocused && inputRef.current){
+      // 4.) ref is passed to the input field's ref attribute, so its current property
+      // gives access to the element. This executes focus as a side-effect, but only
+      // if isFocused is set and current property is existent. 
       inputRef.current.focus();
     }
   }, [isFocused]);
@@ -120,7 +126,10 @@ const InputWithLabel = ({ id, label, value, isFocused, type = 'text', onInputCha
     <>
       <label htmlFor={id}>{children}</label>
       &nbsp;
+      {/* 2.) ref is passed to the input field's JSX-reserved ref attribute 
+        and the element instance is assigned to the changeable current component  */}
       <input 
+        ref={inputRef}
         id={id}
         type={type}
         value={value}
