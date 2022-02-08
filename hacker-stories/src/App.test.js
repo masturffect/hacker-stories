e251c77018d1a313e5742ee307e5792911fc7290
Masturffect <ianseal10@gmail.com>
2022-02-07 23:57:06 -0500

    added testing, styled components, typescript

--- hacker-stories/src/App.test.js
+++ hacker-stories/src/App.test.js
@@ -1,8 +1,45 @@
+import * as React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App, {
+  storiesReducer,
+  Item,
+  List,
+  SearchForm,
+  InputWithLabel
+} from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+const storyOne = {
+  title: 'React',
+  url: 'https://reactjs.org/',
+  author: 'Jordan Walke',
+  num_comments: 3,
+  points: 4,
+  objectID: 0,
+};
+
+const storyTwo = {
+  title: 'Redux',
+  url: 'https://reactjs.org/',
+  author: 'Dan Abramov, Andrew Clark',
+  num_comments: 2,
+  points: 5,
+  objectID: 1,
+};
+
+const stories = [storyOne, storyTwo];
+
+describe('storiesReducer', () => {
+  test('removes a story from all stories', () => {
+    
+  })
+})
+
+describe('something truthy and falsy', () => {
+  test('true to be true', () => {
+    expect(true).toBeTruthy();
+  });
+
+  test('false to be false', () => {
+    expect(false).toBeFalsy();
+  });
+})
